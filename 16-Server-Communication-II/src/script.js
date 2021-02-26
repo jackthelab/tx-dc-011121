a6415a996437168a59402b0003dc788b7b7694b7
Jacob Hampton <jhampton792@gmail.com>
2021-02-26 12:00:52 -0600

    Lecture notes added

--- 16-Server-Communication-II/src/script.js
+++ 16-Server-Communication-II/src/script.js
@@ -17,104 +17,120 @@ const BASE_URL = 'http://localhost:3000/pokemon/'
     // Remove that pokemon from the DOM
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#pokeBtn').addEventListener('click', getPokemon)
-    document.querySelector('form').addEventListener('submit', handleSubmit)
+    document.querySelector('#pokeBtn').addEventListener('click', fetchPokemon)
+    document.getElementById("poke-form").addEventListener('submit', handleSubmit)
 })
 
-//Fetch Function
-const getPokemon = () => {
-    document.querySelector('#pokemon-container').innerHTML = ""
-    fetch(BASE_URL)
-        .then((res) => res.json() )
-        .then(pokeData => pokeData.forEach(renderPokemon))
+// function fetchPokemon(){
+//     fetch(BASE_URL)
+//         .then(res => res.json())
+//         .then(pokeData => {
+//             pokeData.forEach(pokemon => renderPokemon(pokemon));
+//         })
+// }
+
+// above/below are two ways to write same function -- I like the below better, personally
+
+async function fetchPokemon() {
+    const response = await fetch(BASE_URL);
+    const pokeData = await response.json();
+
+    pokeData.forEach(pokemon => renderPokemon(pokemon));
 }
 
-//Finished Render Function
-const renderPokemon = (pokemon) => {
+function renderPokemon(pokemon) {
+    // console.log(pokemon);
+    let newCard = document.createElement('div');
+        newCard.classList.add('card', 'm-2');
 
-    let card = document.createElement('div')
-        card.classList.add('card', 'm-2')
-
-    let img = document.createElement('img')
-        img.className = 'card-img-top'
-        img.src = pokemon.sprite
+    let cardImg = document.createElement('img');
+        cardImg.src = pokemon.sprite
+        cardImg.className = 'card-img-top'
 
     let cardBody = document.createElement('div')
         cardBody.classList.add('card-body')
 
     let cardTitle = document.createElement('h5')
+        cardTitle.innerText = pokemon.name
         cardTitle.classList.add('card-title')
-        cardTitle.textContent = pokemon.name
-
-    let commentList = document.createElement('ul')
-        commentList.classList.add('list-group', 'list-group-flush')
-
+    
     let likeBtn = document.createElement('button')
         likeBtn.innerText = `Likes: ${pokemon.likes}`
         likeBtn.classList.add('btn', 'btn-primary')
         likeBtn.id = pokemon.id
         likeBtn.addEventListener('click', likePokemon)
-
+    
     let delBtn = document.createElement('button')
         delBtn.innerText = "Let Go"
-        delBtn.classList.add('btn', 'btn-danger')
+        delBtn.classList.add('btn', 'btn-danger');
         delBtn.addEventListener('click', () => {
-            delPokemon(pokemon, card)
-          
+            releasePokemon(pokemon, newCard);
         })
-    // pokemon.comments.forEach(comment => {
-    //     let newCom = document.createElement('li')
-    //         newCom.innerText = comment
-    //         newCom.classList.add('list-group-item')
-    //     commentList.appendChild(newCom)
-    // })
+     
+    cardBody.append(cardTitle, likeBtn, delBtn);
 
-    cardBody.append(cardTitle, commentList, likeBtn, delBtn)
-    card.append(img, cardBody)
+    newCard.append(cardImg, cardBody);
 
-    document.querySelector('#pokemon-container').appendChild(card)
+    document.getElementById("pokemon-container").appendChild(newCard);
 }
 
-// Like a pokemon
-function likePokemon(event){
- 
+function handleSubmit(event) {
+    event.preventDefault();
+// using the event.target.*namefromHTMLelement*.value instead of traditional syntax
+    // console.log(event.target.pokeName.value)
+    // console.log(event.target.pokeImg.value)
+    let newPokemon = {
+        name: event.target.pokeName.value,
+        likes: 0,
+        sprite: event.target.pokeImg.value,
+        comments: []
+    }
+
+    let reqObj = {
+        headers: {"Content-Type": "application/json"},
+        method: "POST",
+        body: JSON.stringify(newPokemon)
+    }
+
+    // debugger
+
+    fetch(BASE_URL, reqObj)
+        .then(res => res.json())
+        .then(renderPokemon);
+
+    //Not sure if the below works right... 
+
+    // async function addPokemon () {
+    //     let response = await fetch(BASE_URL, reqObj);
+    //     let pokeToAdd = await response.json()
+
+    //     renderPokemon(pokeToAdd);
+    // }
+}
+
+function likePokemon(event) {
+    console.log(event.target.id)
+    console.log(+event.target.innerText.split(" ")[1]);
+
     let newLikes = {
         likes: +event.target.innerText.split(" ")[1] + 1
     }
+
+    // console.log(newLikes);
 
     let reqObj = {
         headers: {"Content-Type": "application/json"},
         method: "PATCH",
         body: JSON.stringify(newLikes)
     }
-    fetch(BASE_URL + event.target.id, reqObj)
-        .then(r => r.json())
+
+    fetch(BASE_URL+event.target.id, reqObj)
+        .then(res => res.json())
         .then(updatedPokemon => {
             document.getElementById(updatedPokemon.id).innerText = `Likes: ${updatedPokemon.likes}`
         })
 }
 
-// Create a new pokemon
-function handleSubmit(e){
-    e.preventDefault()
-    let newPokemon = {
-        name: e.target.pokeName.value,
-        likes: 0,
-        sprite: e.target.pokeImg.value,
-        comments: []
-    }
-    let reqObj = {
-        headers: {"Content-Type": "application/json"},
-        method: "POST",
-        body: JSON.stringify(newPokemon)
-    }
-    fetch(BASE_URL, reqObj)
-        .then(r => r.json())
-        .then(renderPokemon)
-}
-
-// delete a pokemon
-function delPokemon(pokemon, card){
-    fetch(BASE_URL+pokemon.id, {method: "DELETE"})
-        .then(() => card.remove())
+function releasePokemon(pokemon, card) {
+    fetch(BASE_URL+pokemon.id, {method: "DELETE"}).then(() => card.remove());
 }
